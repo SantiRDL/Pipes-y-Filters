190830a7cb7759391a50dbbe15d22dee36f533f2
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2022-05-31 19:32:16 -0300

    a

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,26 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+
+            //IMPORTANTE: Se implementa el proceso al revés de como aparece en el diagrama!!!
+            IPipe pipenull = new PipeNull();
+            IFilter negativo = new FilterNegative(); 
+            IPipe pipe2 = new PipeSerial(negativo, pipenull);
+            IFilter gris = new FilterGreyscale();
+            IPipe pipe1 = new PipeSerial(gris, pipe2);
+
+            picture= pipe1.Send(picture);           
+
+            provider.SavePicture(picture, @"beermodificado.jpg");
+
+            }
+           // IPicture picture = provider.SavePicture("PathToNewImage.jpg");
+
+           IFilter persistFilter = new 
+
         }
     }
 }
